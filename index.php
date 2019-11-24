fd9713fa0ef2999da53ed4cdcfe2166c0e2595cb
Victor Manuel Echavarria <vicme93@gmail.com>
2019-11-24 13:14:54 -0500

    trabajo php

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<meta charset="utf -8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>
+    Ejemplo pagina Responsive Design
+</title>
+<link rel="stylesheet" type="text/css" href="css/estilos.css">
+
+</head>
+<body>
+   <div id="pagewrap">
+    <div id="header">
+        <div id="info">
+        <h1>Black Friday</h1>
+        <p>Aprovecha nuestras promociones de fin de semana</p>
+        </div>
+        <div id="logo"></div>
+        <img class="imagen1" src="img/Black-friday.jpg">
+        
+    </div>
+    
+    <div id="content">
+        <h2>Contenido</h2>
+        <p>Parrafo 1</p>
+        <p>Parrafo 2</p>
+        <p>Parrafo 3</p>
+        <p>Parrafo 4</p>
+        <p>Parrafo 5</p>
+        <p>Parrafo 6</p>
+    </div>
+    <div id="sidebar">
+        <h3>Sidebar</h3>
+        <p>Texto 1</p>
+        <p>Texto 2</p>
+        <p>Texto 3</p>
+    </div>
+<div id="footer">
+    <h4>Footer</h4>
+</div>
+
+
+   </div>
+</body>
+
+
+
+</html>
